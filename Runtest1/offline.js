dd77c70c35b2e7c164c028e5f9d5944a683fd723
Athicha Santi <athicha@kkumail.com>
2021-10-07 14:26:31 +0700

    GameRun

---
+++ Runtest1/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1633590047,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/furit-sheet0.png",
+		"images/playbotton-sheet0.png",
+		"images/lefbutt-sheet0.png",
+		"images/rightbutt-sheet0.png",
+		"images/upbutt-sheet0.png",
+		"images/reoplaybutt-sheet0.png",
+		"media/mario coin sound - sound effect (hd).m4a",
+		"media/mario coin sound - sound effect (hd).ogg",
+		"media/melodyloops-original-the-first-light-2m30s.m4a",
+		"media/melodyloops-original-the-first-light-2m30s.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
